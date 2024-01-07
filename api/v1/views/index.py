8379cdc942807883c8160d59864ca4fb91ea4a92
Ato Toffah <atotoffah@gmail.com>
2024-01-07 20:28:15 +0000

    4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ Index blueprint definition
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 # @app_views.get("/status")
@@ -11,3 +12,17 @@ from flask import jsonify
 def get_status():
     """Gets the API status"""
     return jsonify(status="OK")
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """Retrieves the number of each objects by type"""
+    stats = {}
+    stats["amenities"] = storage.count("Amenity")
+    stats["cities"] = storage.count("City")
+    stats["places"] = storage.count("Place")
+    stats["reviews"] = storage.count("Review")
+    stats["states"] = storage.count("State")
+    stats["users"] = storage.count("User")
+
+    return jsonify(stats)
