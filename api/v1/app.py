12c150d58220faf58b5e0fe01c73ff113d2030f5
Ato Toffah <atotoffah@gmail.com>
2024-01-07 20:04:59 +0000

    3. Status of your API:Fix 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,13 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
+@app.teardown_appcontext
+def teardown_app(exception):
+    """The Flask app/request context end event listener"""
+    storage.close()
+
+
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(getenv('HBNB_API_PORT', '5000'))
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, threaded=True)
