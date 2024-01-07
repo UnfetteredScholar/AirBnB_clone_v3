23fe76db3185e7d33469e48d64dfba2900938692
Ato Toffah <atotoffah@gmail.com>
2024-01-07 19:48:49 +0000

    3. Status of your API

---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Index blueprint definition
+"""
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.get("/status")
+def get_status():
+    """Gets the API status"""
+    return jsonify(status="OK")
