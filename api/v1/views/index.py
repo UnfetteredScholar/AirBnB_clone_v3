6dc702fe17f55fe823fda066dfac1a0947bd1ac7
Ato Toffah <atotoffah@gmail.com>
2024-01-07 20:18:16 +0000

    Fix 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,7 +6,8 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.get("/status")
+# @app_views.get("/status")
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
     """Gets the API status"""
     return jsonify(status="OK")
